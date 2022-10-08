51adfff3d387d4f0ee33422d1e3d222443735593
Bui Bao Hoang <89177013+buibaohoang06@users.noreply.github.com>
2022-10-09 06:45:48 +0700

    Version 1.0.0

--- r34downloader.py
+++ r34downloader.py
@@ -3,35 +3,46 @@ import json
 import os
 import wget
 import sys
+import time
+
 
 print("""
 =======================================================================================
-=                         Rule34 Image Downloader CLI. v0.0.1                         =  
+=                         Rule34 Image Downloader CLI. v1.0.0                         =  
 =======================================================================================
 
 ! MAKE SURE TO PLACE THE SCRIPT INSIDE THE FOLDER YOU WISH TO INSTALL THE IMAGES INTO !
 """)
 
 try:
-    print("> Please enter all the required information below: ")
-    tag = input("> Enter the tag (if there are multiple, seperate them with a ',' no spaces): ")
-    limit = input("> Number of images: ")
+    print("[Info] > Please enter all the required information below: ")
+    tag = input("[Info] > Enter the tag (if there are multiple, seperate them with a space): ")
     #Gather info from tag
-    print("\n> Communicating with Rule34 server...")
-    query_string = tag.replace(",", "%20")
-    print("Gathering info with tag: " + tag)
-    with urllib.request.urlopen(f'https://api.rule34.xxx/index.php?page=dapi&s=post&q=index&tags={query_string}&json=1&limit={limit}') as data:
+    print("\n[Info] > Communicating with Rule34 server...")
+    query_string = tag.replace(" ", "%20")
+    print("[Info] > Gathering info with tag: " + tag)
+    with urllib.request.urlopen(f'https://api.rule34.xxx/index.php?page=dapi&s=post&q=index&tags={query_string}&json=1') as data:
         json_data = json.loads(data.read().decode())
 
-    images = []
-
+    
+    print("[Info] > Gathering info complete!\n")
+    print(f"[Info] > Found {str(len(json_data))} results with the tags '{tag}'\n")
+    print("[Info] > Starting download script...")
+    print("[Warning] > The script will keep downloading images that it can find, press Ctrl + C to stop when the desirable amount is met.")
+    time.sleep(5)
+    count = 0
     for i in range(0, len(json_data)):
-        images.append(json_data[i]['file_url'])
-    print("> Gathering info complete!\n")
-    print(f"> Found {str(len(json_data))} results out of {limit} set by user with the tags '{tag}'\n")
-    print("> Starting download script...")
-    for links in images:
-        wget.download(links, os.getcwd())
-    sys.exit("> Download completed!")
+        if os.path.exists(json_data[i]['image']):
+            continue
+        else:
+            count = count + 1
+            print(f"[Info] > Downloading #{count}: " + json_data[i]['hash'] + "\n")
+            wget.download(json_data[i]['file_url'], os.getcwd())
+            print("\n")
+            time.sleep(1)
+    print("\n[Info] > Download completed!")
+    sys.exit("\nProgram ran with 0 errors.")
+except KeyError:
+    print("[Error] > Cannot find images with the provided keywords.")
 except Exception as e:
-    print("> Error occured: " + str(e))
+    print("[Error] > " + str(e))
